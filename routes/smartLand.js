7f7c95ad33b2f549f25434cd1fc823d16015e1e6
Patrick <haidu.patrick@gmail.com>
2025-07-06 16:34:43 +0300

    first commit

--- routes/smartLand.js
+++ routes/smartLand.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { contact } = require('../controllers/smartlandController');
+const { contact } = require('../controllers/smartLandController');
 
 router.post('/contact', contact);
 
